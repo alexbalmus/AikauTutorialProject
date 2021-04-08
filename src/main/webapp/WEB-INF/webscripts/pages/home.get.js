1fe6217ad406048cd43c1e011763d43bd6523bc6
alexbalmus <alexbalmus@gmail.com>
2021-04-08 17:26:05 +0300

    Tutorial 3 - Create a Composite Widget

--- src/main/webapp/WEB-INF/webscripts/pages/home.get.js
+++ src/main/webapp/WEB-INF/webscripts/pages/home.get.js
@@ -14,67 +14,11 @@ model.jsonModel = {
          config: 
          {
             widgets: [
+               // COMPOSITE WIDGET ADDED HERE...
                {
-                  id: "HEADER_BAR",
-                  name: "alfresco/header/Header",
-                  config: {
-                     widgets: [
-                        {
-                           id: "APP_MENU_BAR",
-                           name: "alfresco/header/AlfMenuBar",
-                           align: "left",
-                           config: {
-                              widgets: [
-                                 {
-                                    id: "HOME",
-                                    name: "alfresco/menus/AlfMenuBarItem",
-                                    config: {
-                                       label: "Home",
-                                       targetUrl: "ap/ws/home"
-                                    }
-                                 }
-                              ]
-                           }
-                        },
-                        {
-                           id: "USER_MENU_BAR",
-                           name: "alfresco/header/AlfMenuBar",
-                           align: "right",
-                           config: {
-                              widgets: [
-                                 {
-                                    id: "USER_MENU",
-                                    name: "alfresco/header/AlfMenuBarPopup",
-                                    config: {
-                                       label: "User Menu",
-                                       widgets: [
-                                          {
-                                             id: "HEADER_USER_MENU",
-                                             name: "alfresco/menus/AlfMenuGroup",
-                                             config: {
-                                                widgets: [
-                                                   {
-                                                      id: "LOGOUT",
-                                                      name: "alfresco/header/AlfMenuItem",
-                                                      config:
-                                                      {
-                                                         label: "Logout",
-                                                         iconClass: "alf-user-logout-icon",
-                                                         publishTopic: "ALF_DOLOGOUT"
-                                                      }
-                                                   }
-                                                ]
-                                             }
-                                          }
-                                       ]
-                                    }
-                                 }
-                              ]
-                           }
-                        }
-                     ]
-                  }
+                  name: "tutorial/Header"
                },
+               // THIS IS THE REST OF THE ORIGINAL JSON MODEL...
                {
                   id: "HEADER_TITLE_BAR",
                   name: "alfresco/layout/LeftAndRight",
