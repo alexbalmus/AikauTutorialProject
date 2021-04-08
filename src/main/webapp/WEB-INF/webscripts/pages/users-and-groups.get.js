93488ad9b6070859dc514f133699deb18ab3cbf1
alexbalmus <alexbalmus@gmail.com>
2021-04-08 20:24:27 +0300

    Tutorial 7 - Form Dialogs

--- src/main/webapp/WEB-INF/webscripts/pages/users-and-groups.get.js
+++ src/main/webapp/WEB-INF/webscripts/pages/users-and-groups.get.js
@@ -1,8 +1,54 @@
 model.jsonModel = {
     services: [
-        "alfresco/services/CrudService"
+        "alfresco/services/CrudService",
+        "alfresco/services/DialogService",
+        "tutorial/UserAndGroupService"
     ],
-    widgets:[
+    widgets: [
+        {
+            name: "alfresco/buttons/AlfButton",
+            config: {
+                label: "Create New Group",
+                publishTopic: "ALF_CREATE_FORM_DIALOG_REQUEST",
+                publishPayload: {
+                    dialogTitle: "Create Group",
+                    dialogConfirmationButtonTitle: "Create",
+                    dialogCancellationButtonTitle: "Cancel",
+                    // formSubmissionTopic: "ALF_CRUD_CREATE",
+                    formSubmissionTopic: "TUTORIAL_CREATE_GROUP",
+                    fixedWidth: true,
+                    widgets: [
+                        {
+                            name: "alfresco/forms/controls/TextBox",
+                            config: {
+                                fieldId: "ID",
+                                label: "Identifier",
+                                name: "groupId",
+                                description: "Enter a unique identifier for the group. Only alphanumeric characters are allowed",
+                                requirementConfig: {
+                                    initialValue: true
+                                },
+                                validationConfig: [
+                                    {
+                                        validation: "regex",
+                                        regex: "^[A-Za-z0-9]+$",
+                                        errorMessage: "Alphanumeric characters only"
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            name: "alfresco/forms/controls/TextBox",
+                            config: {
+                                fieldId: "DISPLAYNAME",
+                                label: "Display name",
+                                name: "displayName"
+                            }
+                        }
+                    ]
+                }
+            }
+        },
         {
             name: "alfresco/lists/AlfList",
             config: {
